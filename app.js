3d936397dd5676f606863173a1d109f6a5d4fc63
Makayla Clausen <mclausen792@gmail.com>
2018-01-16 11:10:53 -0700

    Working on image opacity

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('hello');
+var item = document.getElementsByClassName('middle')[0];
+// item.addEventListener("mouseover", func);
+// item.addEventListener("mouseout", func1);
+
+
+function func()
+{
+   document.getElementById("text").style.display="block";
+   document.getElementById("text").style.opacity="1";
+}
+
+
+
+function func1()
+{
+    document.getElementById("text").style.display="none"
+
+}
